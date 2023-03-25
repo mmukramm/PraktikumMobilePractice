d917259e2b9559aac11875dc899417913604e884
MuhammadMukram <mukrammuhammad@gmail.com>
2023-03-25 23:36:44 +0800

    Make user can add profile picture from gallery

--- T3/T3_01/app/src/main/java/com/example/t3_01/MainActivity.java
+++ T3/T3_01/app/src/main/java/com/example/t3_01/MainActivity.java
@@ -1,27 +1,56 @@
 package com.example.t3_01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-    Button submitBtn;
     private ImageView userProfileImg;
-    EditText captionForm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submitBtn = findViewById(R.id.submitBtn);
+        Button submitBtn = findViewById(R.id.submitBtn);
         userProfileImg = findViewById(R.id.userProfileImg);
-
-        submitBtn.setOnClickListener(v -> {
-            startActivity(new Intent(this, MakePostActivity.class));
+        userProfileImg.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            mGetContent.launch(intent);
         });
-
+        submitBtn.setOnClickListener(v -> startActivity(new Intent(this, MakePostActivity.class)));
     }
+
+    ActivityResultLauncher<Intent> mGetContent = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        assert data != null;
+                        Uri selectedImage = data.getData();
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                            userProfileImg.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+    );
 }
