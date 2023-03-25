16de313fd1dd1efc058850ff807897d3726708ae
Mukramm_ <mukrammuhammad@gmail.com>
2023-03-25 13:04:47 +0800

    Finish all the layout. Make it compatible to light and dark mode

--- T3/T3_01/app/src/main/java/com/example/t3_01/MainActivity.java
+++ T3/T3_01/app/src/main/java/com/example/t3_01/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.t3_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button submitBtn;
+    private ImageView userProfileImg;
+    EditText captionForm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitBtn = findViewById(R.id.submitBtn);
+        userProfileImg = findViewById(R.id.userProfileImg);
+
+        submitBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, MakePostActivity.class));
+        });
+
     }
 }
--- T3/T3_01/app/src/main/java/com/example/t3_01/MakePostActivity.java
+++ T3/T3_01/app/src/main/java/com/example/t3_01/MakePostActivity.java
@@ -2,13 +2,23 @@ package com.example.t3_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Scroller;
 
 public class MakePostActivity extends AppCompatActivity {
-
+    Button uploadBtn;
+    EditText captionForm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_post);
+        uploadBtn = findViewById(R.id.uploadBtn);
+        captionForm = findViewById(R.id.captionForm);
+        uploadBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, DisplayPostActivity.class));
+        });
     }
 }
